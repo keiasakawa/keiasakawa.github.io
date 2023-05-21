c4458b21ee5fb6237874a93672e1f2205adc74b3
Kei Asakawa <77090883+Kaiyooon@users.noreply.github.com>
2023-05-20 23:28:12 -0700

    Update

---
+++ components/footer.js
@@ -0,0 +1,57 @@
+class Footer extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <style>
+                .footer nav {
+                height: 50px;
+                display: flex;
+                margin: auto;
+                background-color:  #fff;
+                align-items: center;
+                justify-content: center;
+            }
+    
+            .footer ul {
+                padding: 0;
+                height: 100%;
+                margin-block-start: 0;
+                margin-block-end: 0;
+            }
+            
+            .footer li {
+                display: inline-block;
+                height: 100%;
+            }
+
+            .footer img {
+                position: relative;
+                top: 15%;
+                height: 30px;
+                width: 30px;
+                margin: 0 25px;
+            }
+
+            footer {
+                position:fixed;
+                bottom: 0;
+                width: 100%;
+            }
+            </style>
+            <Footer class="footer">
+            <nav>
+                <ul>
+                    <li><a class="linkedin" href="https://www.linkedin.com/in/kei-asakawa/"><img src="images/linkedin.png" alt=linkedin></a></li>
+                    <li><a class="github" href="https://github.com/keiasakawa"><img src="images/github.png" alt=github></a></li>
+                </ul>
+            </nav>
+            </Footer>
+        `;
+    }
+
+}
+
+customElements.define('footer-component', Footer);
---
+++ components/header.js
@@ -0,0 +1,60 @@
+class Header extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <style>
+                .header nav {
+                height: 50px;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                background-color:  #444444;
+            }
+    
+            .header ul {
+                padding: 0;
+                margin-bottom: 0;
+            }
+            
+            .header a {
+                font-weight: 700;
+                margin: 0 25px;
+                color: #fff;
+                text-decoration: none;
+                border-radius: 10%;
+            }
+
+            .header a:hover {
+                padding-bottom: 5px;
+                box-shadow: inset 0 -2px 0 0 #fff;
+            }
+            
+            .header li {
+                display: inline-block;
+            }
+
+            .header header{
+                position:fixed;
+                top: 0;
+                width: 100%;
+            }
+
+            </style>
+            <header class="header">
+            <nav>
+                <ul>
+                    <li><div><a href="index.html">About</a></div></li>
+                    <li><a href="projects.html">Projects</a></li>
+                    <li><a href="resume.html">Resume</a></li>
+                </ul>
+            </nav>
+            </header>
+        `;
+    }
+
+}
+
+customElements.define('header-component', Header);
